c96fdf1be4f0bc9fdb64d60c828fc1d3c248bfb6
John Emmons <emmons.john@gmail.com>
2017-07-05 15:17:05 -0700

    added improved glacier OCR

---
+++ glacierdigits2/train.sh
@@ -0,0 +1,5 @@
+mv inttemp glacierdigits2.inttemp
+mv normproto glacierdigits2.normproto
+mv pffmtable glacierdigits2.pffmtable
+mv shapetable glacierdigits2.shapetable
+combine_tessdata glacierdigits2.
