aa8d8f1b1805b141a34f245daf43a1a63f16af3c
John Emmons <emmons.john@gmail.com>
2017-08-07 09:49:19 -0700

    updated run script to account for the completed reels

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for reel_num in $(seq 4 57); do
+for reel_num in $(seq 28 57); do
     echo "processing reel: $reel_num"
     ./antarctica_image_processor --reel "$reel_num" --parallel 48 --flush 1 --output /mnt/data/jemmons/geophysics/finished/Reel_"$reel_num"_output/ /mnt/data/jemmons/geophysics/35mm/Reel_3/TIFF\ \(2400x1800\)/* &> /mnt/data/jemmons/geophysics/finished/Reel_"$reel_num"_output.log
 done
